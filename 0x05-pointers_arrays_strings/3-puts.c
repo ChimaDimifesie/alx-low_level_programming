9a42258792df0cb1aa35019861a42480b307e91f
ChimaDimifesie <chimadimifesie1@gmail.com>
2023-03-03 07:45:06 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @str: the str to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	putchar(*str);
+	}
+	putchar('\n');
+}
