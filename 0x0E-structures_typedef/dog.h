1579633138c25b539389b7f46a56dac051e57f07
ChimaDimifesie <chimadimifesie1@gmail.com>
2023-03-20 17:37:55 -0700

    1-init_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 dog_t *new_dog(char *name, float age, char *owner);
